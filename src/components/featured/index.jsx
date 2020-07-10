e853a8b426913e9a7b98aef17571307c7d3b1629
Hữu Tôn <nghuuton@gmail.com>
2020-07-10 12:28:17 +0700

    Carrousel

---
+++ src/components/featured/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Carrousel from "./Carrousel";
+
+export default function Featured() {
+  return (
+    <div style={{ position: "relative" }}>
+      <Carrousel />
+      <div className="artist_name">
+        <div className="wrapper">Arina Grande</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/header_footer/sideDrawer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+export default function SideDrawer(props) {
+  return (
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => props.onClose(false)}
+    >
+      <List component="nav">
+        <ListItem button onClick={() => console.log("Feature")}>
+          Event starts in
+        </ListItem>
+
+        <ListItem button onClick={() => console.log("Venue NFO")}>
+          Venue NFO
+        </ListItem>
+
+        <ListItem button onClick={() => console.log("Highlights")}>
+          Highlights
+        </ListItem>
+
+        <ListItem button onClick={() => console.log("Pricing")}>
+          Pricing
+        </ListItem>
+
+        <ListItem button onClick={() => console.log("Location")}>
+          Location
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+}
